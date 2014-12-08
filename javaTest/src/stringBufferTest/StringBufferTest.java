5aaa065a80f08de8bbd254e59419b7aad632b89e
hyh122 <1215605211@qq.com>
2014-12-08 18:15:45 +0800

    first commit 2014/12/08 18:15

---
+++ javaTest/src/stringBufferTest/StringBufferTest.java
@@ -0,0 +1,20 @@
+package stringBufferTest;
+
+public class StringBufferTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * StringBuffer4种构造函数
+		 * StringBuffer()
+		 * StringBuffer(int size)
+		 * StringBuffer(String str)
+		 * StringBuffer(CharSequence chars)
+		 */
+		StringBuffer sb=new StringBuffer("hello");
+		
+		int capacity=sb.length();
+		System.out.println(sb.substring(1));
+	}
+
+}
